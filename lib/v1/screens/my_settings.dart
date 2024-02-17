3c6f8459547496f5bf4b93e10089bbea152c60fb
RonnieZad <zadcorna@gmail.com>
2024-02-17 07:16:22 +0300

    page clean up

--- lib/v1/screens/my_settings.dart
+++ lib/v1/screens/my_settings.dart
@@ -3,18 +3,20 @@
 //  Created by Ronald Zad Muhanguzi .
 //  2023, Enyumba App. All rights reserved.
 
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:lucide_icons/lucide_icons.dart';
-import 'package:vybe/v1/controllers/auth_controller.dart';
-import 'package:vybe/v1/utils/extensions.dart';
-import 'package:vybe/v1/utils/typography.dart';
-import 'package:vybe/v1/widgets/app_background.dart';
-import 'package:vybe/v1/widgets/header.dart';
-import 'package:vybe/v1/widgets/settings_container.dart';
-import 'package:vybe/v1/widgets/settings_row.dart';
+import 'package:zula/v1/controllers/auth_controller.dart';
+import 'package:zula/v1/utils/extensions.dart';
+import 'package:zula/v1/utils/typography.dart';
+import 'package:zula/v1/widgets/app_background.dart';
+import 'package:zula/v1/widgets/header.dart';
+import 'package:zula/v1/widgets/screen_overlay.dart';
+import 'package:zula/v1/widgets/settings_container.dart';
+import 'package:zula/v1/widgets/settings_row.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MySettings extends StatefulWidget {
@@ -32,7 +34,7 @@ class _MySettingsState extends State<MySettings> {
 
   @override
   Widget build(BuildContext context) {
-    AuthController _authController = Get.find();
+    AuthController authController = Get.find();
     return Scaffold(
       body: Stack(
         children: [
@@ -40,11 +42,11 @@ class _MySettingsState extends State<MySettings> {
           Padding(
             padding: EdgeInsets.only(top: 150.h),
             child: ListView(
-                padding: EdgeInsets.only(bottom: 130.h),
+                padding: EdgeInsets.only(bottom: 100.h),
                 physics: const BouncingScrollPhysics(),
                 children: <Widget>[
                   const SettingsWidget(
-                    title: 'Help',
+                    title: '',
                     widget: Column(
                       children: [
                         SettingsRow(
@@ -76,63 +78,13 @@ class _MySettingsState extends State<MySettings> {
                         color: Colors.white10,
                         child: label(text: 'Log out'),
                         onPressed: () {
-                          showDialog(
-                              context: context,
-                              barrierColor: Colors.black87,
-                              builder: (context) {
-                                return Dialog(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(10.r)),
-                                  backgroundColor: Colors.black,
-                                  child: Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 20.w, vertical: 15.h),
-                                    child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          heading(
-                                              text: 'Test here',
-                                              color: Colors.white),
-                                          10.ph,
-                                          paragraph(
-                                              text:
-                                                  'Confirm whether you would like to log out',
-                                              color: Colors.white60),
-                                          25.ph,
-                                          Row(
-                                            children: [
-                                              Expanded(
-                                                child: CupertinoButton(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          50.0),
-                                                  color: Colors.white10,
-                                                  child: label(text: 'Log out'),
-                                                  onPressed: () {
-                                                    _authController
-                                                        .guestLogout();
-                                                    Navigator.pop(context);
-                                                  },
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          10.ph,
-                                          Center(
-                                              child: TextButton(
-                                                  onPressed: () {
-                                                    Navigator.pop(context);
-                                                  },
-                                                  child: label(
-                                                      text: 'Cancel',
-                                                      color: Colors.white60)))
-                                        ]),
-                                  ),
-                                );
-                              });
+                          ScreenOverlay.showConfirmationDialog(context,
+                              title: 'Log out',
+                              description:
+                                  'Confirm whether you would like to log out',
+                              action: () {
+                            authController.guestLogout();
+                          });
                         }),
                   ),
                   50.ph,
@@ -144,7 +96,7 @@ class _MySettingsState extends State<MySettings> {
                   10.ph,
                   Center(
                       child: paragraph(
-                          text: 'Version: 1.0.0 Beta', color: Colors.white30))
+                          text: 'Version: 1.0.1+3 Beta', color: Colors.white30))
                 ]),
           ),
           const Header(
