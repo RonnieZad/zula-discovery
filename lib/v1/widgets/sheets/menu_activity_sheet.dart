e2fc1aa10226e1a64125363414c814ca8c7999b1
RonnieZad <zadcorna@gmail.com>
2024-02-17 07:18:07 +0300

    chnaged data source to api

--- lib/v1/widgets/sheets/menu_activity_sheet.dart
+++ lib/v1/widgets/sheets/menu_activity_sheet.dart
@@ -1,62 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:vybe/v1/models/dish_menu_model.dart';
-import 'package:vybe/v1/screens/explore_page.dart';
-import 'package:vybe/v1/utils/extensions.dart';
-import 'package:vybe/v1/utils/typography.dart';
+import 'package:zula/v1/models/location_model.dart';
+import 'package:zula/v1/utils/extensions.dart';
+import 'package:zula/v1/utils/typography.dart';
 
 class MenuActivitySheet extends StatelessWidget {
   const MenuActivitySheet({
     super.key,
-    required this.menuItems,
+    required this.locationMenuActivity,
   });
 
-  final List<Dish> menuItems;
+  final List<LocationMenuActivity> locationMenuActivity;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment:
-          CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: 20.w),
-          child: heading(
-              text: 'Menu',
-              fontSize: 27.sp,
-              color: Colors.white),
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: heading(text: 'Menu', fontSize: 27.sp, color: Colors.white),
         ),
         20.ph,
         // Add your menu items here of dishes, put pictures, name, description and price
         ListView.builder(
           padding: EdgeInsets.zero,
           shrinkWrap: true,
-          itemCount: menuItems.length,
+          itemCount: locationMenuActivity.length,
           itemBuilder: (context, index) {
             return ListTile(
               leading: ClipRRect(
-                borderRadius:
-                    BorderRadius.circular(6.r),
+                borderRadius: BorderRadius.circular(6.r),
                 child: Image.network(
-                  menuItems[index].imageUrl,
+                  locationMenuActivity[index].locationMenuActivityPictureUrl,
                   width: 60.0,
                   height: 60.0,
                   fit: BoxFit.cover,
                 ),
               ),
               title: heading(
-                  text: menuItems[index].name,
+                  text: locationMenuActivity[index].locationMenuActivityTitle,
                   fontSize: 21.sp,
                   color: Colors.white),
               subtitle: paragraph(
-                  text: menuItems[index]
-                      .description,
+                  text: locationMenuActivity[index]
+                      .locationMenuActivityDescription,
                   color: Colors.white54,
                   fontSize: 19.sp),
               trailing: heading(
                   text:
-                      '\$${menuItems[index].price.toStringAsFixed(2)}',
+                      'UGX${locationMenuActivity[index].locationMenuActivityPrice}',
                   fontSize: 20.sp,
                   color: Colors.white),
             );
@@ -66,4 +59,3 @@ class MenuActivitySheet extends StatelessWidget {
     );
   }
 }
-
