66cac5fc747fc55f86d965870443293dc0beb658
RonnieZad <zadcorna@gmail.com>
2024-02-17 07:17:58 +0300

    added link parser to handle urls

---
+++ lib/v1/utils/link_parser.dart
@@ -0,0 +1,54 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkParser {
+  LinkParser._();
+  static void launchGoolgeMapsNavigation(
+      double latitude, double longitude) async {
+    final url =
+        'https://www.google.com/maps?q=${latitude},${longitude}&travelmode=driving';
+
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+   static void launchWhatsappHelpCenter(
+      ) async {
+    const url =
+        'https://wa.me/256702703612?text=Hi Zula Team, I need assistance on.....';
+
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+   static void launchWhatsappWithLink(String? link) async {
+    final facebookUrl = 'https://api.whatsapp.com/send?text=$link';
+    if (link != null) {
+      if (await canLaunchUrl(Uri.parse(facebookUrl))) {
+        await launchUrl(Uri.parse(facebookUrl));
+      } else {
+        throw 'Could not launch $facebookUrl';
+      }
+    } else {
+     
+    }
+  }
+
+  static void launchSMSWithLink(String? link) async {
+    final smsUrl = 'sms:?body=$link';
+    if (link != null) {
+      if (await canLaunchUrl(Uri.parse(smsUrl))) {
+        await launchUrl(Uri.parse(smsUrl));
+      } else {
+        throw 'Could not launch $smsUrl';
+      }
+    } else {
+     
+    }
+  }
+}
