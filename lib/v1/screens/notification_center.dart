40237913de95d9d05db2dba44041b1265c079d26
RonnieZad <zadcorna@gmail.com>
2024-02-17 07:16:35 +0300

    page clean up

--- lib/v1/screens/notification_center.dart
+++ lib/v1/screens/notification_center.dart
@@ -4,9 +4,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:line_icons/line_icon.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:lucide_icons/lucide_icons.dart';
-import 'package:vybe/v1/utils/extensions.dart';
-import 'package:vybe/v1/utils/typography.dart';
-import 'package:vybe/v1/widgets/app_background.dart';
+import 'package:zula/v1/utils/extensions.dart';
+import 'package:zula/v1/utils/typography.dart';
+import 'package:zula/v1/widgets/app_background.dart';
 
 class NotificationCenter extends StatefulWidget {
   const NotificationCenter({super.key});
