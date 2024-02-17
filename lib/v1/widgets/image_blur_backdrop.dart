31468d52af6da63ce29ebd4469734ed7663fd627
RonnieZad <zadcorna@gmail.com>
2024-02-17 07:18:49 +0300

    code clean up

--- lib/v1/widgets/image_blur_backdrop.dart
+++ lib/v1/widgets/image_blur_backdrop.dart
@@ -2,7 +2,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:octo_image/octo_image.dart';
 
 class BackgrounBlurView extends StatelessWidget {
   final String imageUrl;
@@ -30,5 +32,16 @@ class BackgrounBlurView extends StatelessWidget {
             width: double.infinity,
           ))
     ]);
+  }
+}
+
+class OctoBlurHashFix {
+  static OctoPlaceholderBuilder placeHolder(String hash, {BoxFit? fit}) {
+    return (context) => SizedBox.expand(
+          child: Image(
+            image: BlurHashImage(hash),
+            fit: fit ?? BoxFit.cover,
+          ),
+        );
   }
 }
