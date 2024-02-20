da465f045e06297bd9e9463bba2c2a5b778da1fd
RonnieZad <zadcorna@gmail.com>
2024-02-20 17:12:32 +0300

    added cached netowrk image to imporove experience

--- lib/v1/widgets/image_blur_backdrop.dart
+++ lib/v1/widgets/image_blur_backdrop.dart
@@ -1,6 +1,6 @@
-
 import 'dart:ui';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -17,17 +17,21 @@ class BackgrounBlurView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(children: [
       SizedBox(
-        height: 500.h,
-        width: double.infinity,
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
-        ),
-      ),
+          height: 500.h,
+          width: double.infinity,
+          child: OctoImage(
+            placeholderBuilder:
+                OctoBlurHashFix.placeHolder('LEHV6nWB2yk8pyo0adR*.7kCMdnj'),
+            errorBuilder: OctoError.icon(color: Colors.red),
+            image: CachedNetworkImageProvider(
+              imageUrl,
+            ),
+            fit: BoxFit.cover,
+          )),
       BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
           child: Container(
-            color: Colors.black.withOpacity(0.6),
+            color: Colors.white.withOpacity(0.6),
             height: 550.h,
             width: double.infinity,
           ))
