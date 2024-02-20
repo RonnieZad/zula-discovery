1ef905569c2015c0de1df003d75fb21a8168cbf3
RonnieZad <zadcorna@gmail.com>
2024-02-20 17:13:15 +0300

    tweaked top padding

--- lib/v1/widgets/header.dart
+++ lib/v1/widgets/header.dart
@@ -12,15 +12,14 @@ import 'package:line_icons/line_icons.dart';
 import 'package:zula/v1/utils/extensions.dart';
 import 'package:zula/v1/utils/typography.dart';
 
-
 class Header extends StatelessWidget {
-  const Header(
-      {Key? key,
-      required this.titleText,
-      required this.headingText,
-      this.hasBackButton = false,
-      this.bottomWidget})
-      : super(key: key);
+  const Header({
+    Key? key,
+    required this.titleText,
+    required this.headingText,
+    this.hasBackButton = false,
+    this.bottomWidget,
+  }) : super(key: key);
   final String titleText;
   final String headingText;
   final bool? hasBackButton;
@@ -32,64 +31,34 @@ class Header extends StatelessWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
         child: Container(
+          padding: EdgeInsets.only(
+              bottom: 10.h, top: 40.h, left: hasBackButton! ? 20.w : 20.w),
           width: double.infinity,
-          
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Stack(
-                clipBehavior: Clip.none,
+            children: [
+              Row(
                 children: [
-                  // Positioned(
-                  //   top: -100,
-                  //   child: ShaderMask(
-                  //     shaderCallback: (bounds) {
-                  //       return const LinearGradient(
-                  //         begin: Alignment.topCenter,
-                  //         end: Alignment.bottomCenter,
-                  //         stops: [0.2, 0.79],
-                  //         colors: [Colors.white, Colors.transparent],
-                  //       ).createShader(bounds);
-                  //     },
-                  //     // blendMode: BlendMode.dstATop,
-                  //     child: SvgPicture.asset(
-                  //       'assets/images/pat.svg',
-                  //       width: 500.w,
-                  //       // color: Theme.of(context).primaryColor,
-                  //     ),
-                  //   ),
-                  // ),
-
-                  //           padding:
-                  // EdgeInsets.only(left: 20.w, right: 20.w, top: 40.h, bottom: 10.h),
-                  Padding(
-                      padding: EdgeInsets.only(
-                          top: 60.h, left: hasBackButton! ? 20.w : 20.w),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              hasBackButton!
-                                  ? GestureDetector(
-                                      onTap: () {
-                                        HapticFeedback.lightImpact();
-                                        Navigator.pop(context);
-                                      },
-                                      child: Icon(LineIcons.arrowLeft))
-                                  : const SizedBox.shrink(),
-
-                                  hasBackButton! ? 10.pw : 0.ph,
-                              title(text: titleText, fontSize: 35.sp, color: Colors.white),
-                            ],
-                          ),
-                          5.ph,
-                          heading(text: headingText, color: Colors.white60),
-                        ],
-                      )),
+                  hasBackButton!
+                      ? GestureDetector(
+                          onTap: () {
+                            HapticFeedback.lightImpact();
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            LineIcons.arrowLeft,
+                            color: Colors.white,
+                          ))
+                      : const SizedBox.shrink(),
+                  hasBackButton! ? 10.pw : 0.ph,
+              titleText.isEmpty ? const SizedBox.shrink():    heading(
+                      text: titleText, fontSize: 38.sp, color: Colors.white),
                 ],
               ),
+              10.ph,
+              headingText.isEmpty ? const SizedBox.shrink():  
+              heading(text: headingText, color: Colors.white70),
             ],
           ),
         ),
