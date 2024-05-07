1521054d1d2256faf54b5d2a56b58c319ca90490
RonnieZad <zadcorna@gmail.com>
2024-05-07 04:01:35 +0300

    added date and getNotificationIcon helper util

--- lib/v1/utils/helper.dart
+++ lib/v1/utils/helper.dart
@@ -1,4 +1,6 @@
 import 'package:intl/intl.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
 
 class Helper {
   Helper._();
@@ -20,5 +22,34 @@ class Helper {
     }
 
     return textToFormat;
+  }
+
+    static String getDate(DateTime textToFormat) {
+    return  DateFormat('MMM dd, yyyy').format(textToFormat);
+  }
+
+  static getNotificationIcon(String type) {
+    switch (type.toLowerCase()) {
+      case 'app_update':
+        return LineIcons.download;
+
+      case 'pending':
+        return LineIcons.download;
+
+      case 'failed':
+        return LineIcons.download;
+
+      case 'processed_with_errors':
+        return LineIcons.download;
+
+      case 'matured':
+        return LineIcons.download;
+
+      case 'processed':
+        return LineIcons.download;
+
+      default:
+        return LineIcons.download;
+    }
   }
 }
