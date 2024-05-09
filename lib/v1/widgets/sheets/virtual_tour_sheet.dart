8d00c4ddf7ee29b3f883a501d3ba78901902b064
RonnieZad <zadcorna@gmail.com>
2024-05-10 00:57:38 +0300

    added loading widget as page is initiating

--- lib/v1/widgets/sheets/virtual_tour_sheet.dart
+++ lib/v1/widgets/sheets/virtual_tour_sheet.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:zula/v1/utils/extensions.dart';
+import 'package:zula/v1/utils/typography.dart';
 
 class VirtualTourSheet extends StatefulWidget {
   const VirtualTourSheet({
@@ -16,33 +19,58 @@ class VirtualTourSheet extends StatefulWidget {
 
 class _VirtualTourSheetState extends State<VirtualTourSheet> {
   late WebViewController webViewController;
+  bool showLoader = true;
 
   @override
   void initState() {
-     webViewController = WebViewController()
+    webViewController = WebViewController()
+    
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
         NavigationDelegate(
           onProgress: (int progress) {
-            // Update loading bar.
+            print(progress);
           },
           onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
+          onPageFinished: (String url) {
+            setState(() {
+              showLoader = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            setState(() {
+              showLoader = false;
+            });
+          },
         ),
       )
       ..loadRequest(Uri.parse(widget.virtualTourPreviewUrl));
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: [
         SizedBox(
             height: 1.sh,
             width: double.infinity,
             child: WebViewWidget(controller: webViewController)),
+        showLoader
+            ? Center(
+                child: Column(
+                  children: [
+                    240.ph,
+                    Lottie.asset('splash_animation'.toJson,
+                        width: 100.w, height: 100.w),
+                    10.ph,
+                    paragraph(text: 'Loading, please wait')
+                  ],
+                ),
+              )
+            : const SizedBox.shrink()
       ],
     );
   }
