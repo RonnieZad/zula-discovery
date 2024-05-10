36dae2ebdd35c774d9fc22a525564bfb8fdd1d33
RonnieZad <zadcorna@gmail.com>
2024-05-11 02:01:25 +0300

    changed header right padding

--- lib/v1/widgets/header.dart
+++ lib/v1/widgets/header.dart
@@ -41,7 +41,7 @@ class Header extends StatelessWidget {
           padding: EdgeInsets.only(
               bottom: 10.h,
               top: Platform.isAndroid ? 35.h : 60.h,
-              right: 20.w,
+              right: 5.w,
               left: hasBackButton! ? 20.w : 20.w),
           width: double.infinity,
           child: Column(
