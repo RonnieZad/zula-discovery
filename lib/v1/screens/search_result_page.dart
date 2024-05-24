0a5a7a01f0b18df6d43aaea026daf768ef3dadb7
RonnieZad <zadcorna@gmail.com>
2024-05-25 01:50:34 +0300

    added no content widget for empty results

--- lib/v1/screens/search_result_page.dart
+++ lib/v1/screens/search_result_page.dart
@@ -64,114 +64,116 @@ class _SearchResultPageState extends State<SearchResultPage> {
                           paragraph(text: 'Loading, please wait')
                         ],
                       )
-                    :
-                    locationController
-                                .retrievedLocationSearches.isEmpty ? const NoContentWidget(label: 'No results found') :
-                    
-                    
-                    GridView.builder(
-                            padding: EdgeInsets.only(
-                                left: 20.w, right: 20.w, bottom: 120.h),
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    mainAxisExtent: 270,
-                                    mainAxisSpacing: 15.w,
-                                    crossAxisSpacing: 15.h,
-                                    crossAxisCount: 2),
-                            itemCount: locationController
-                                .retrievedLocationSearches.length,
-                            itemBuilder: (context, index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  HapticFeedback.selectionClick();
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => ExploreDetails(
-                                              locationDetails: locationController
-                                                      .retrievedLocationSearches[
-                                                  index])));
-                                },
-                                child: Stack(
-                                  clipBehavior: Clip.none,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(10.r),
-                                      child: SizedBox(
-                                        width: double.infinity,
-                                        child: Stack(
-                                          children: [
-                                            OctoImage(
-                                              width: double.infinity,
-                                              height: double.infinity,
-                                              placeholderBuilder:
-                                                  OctoBlurHashFix.placeHolder(
-                                                      'LEHV6nWB2yk8pyo0adR*.7kCMdnj'),
-                                              errorBuilder: OctoError.icon(
-                                                  color: Colors.red),
-                                              image: CachedNetworkImageProvider(
-                                                  locationController
-                                                      .retrievedLocationSearches[
-                                                          index]
-                                                      .locationPicture[0]
-                                                      .locationPictureUrl),
-                                              fit: BoxFit.cover,
-                                            ),
-                                            Container(
-                                              height: double.infinity,
-                                              width: double.infinity,
-                                              decoration: const BoxDecoration(
-                                                  gradient: LinearGradient(
-                                                      begin:
-                                                          Alignment.topCenter,
-                                                      end: Alignment
-                                                          .bottomCenter,
-                                                      colors: [
-                                                    Colors.transparent,
-                                                    Colors.black45
-                                                  ])),
-                                            ),
-                                            Positioned(
-                                              bottom: 15.h,
-                                              left: 15.w,
-                                              right: 15.w,
-                                              child: Column(
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.start,
-                                                children: [
-                                                  heading(
-                                                      text: locationController
+                    : locationController.retrievedLocationSearches.isEmpty
+                        ? const NoContentWidget(label: 'No results found')
+                        : GridView.builder(
+                                padding: EdgeInsets.only(
+                                    left: 20.w, right: 20.w, bottom: 120.h),
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                        mainAxisExtent: 270,
+                                        mainAxisSpacing: 15.w,
+                                        crossAxisSpacing: 15.h,
+                                        crossAxisCount: 2),
+                                itemCount: locationController
+                                    .retrievedLocationSearches.length,
+                                itemBuilder: (context, index) {
+                                  return GestureDetector(
+                                    onTap: () {
+                                      HapticFeedback.selectionClick();
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => ExploreDetails(
+                                                  locationDetails:
+                                                      locationController
+                                                              .retrievedLocationSearches[
+                                                          index])));
+                                    },
+                                    child: Stack(
+                                      clipBehavior: Clip.none,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(10.r),
+                                          child: SizedBox(
+                                            width: double.infinity,
+                                            child: Stack(
+                                              children: [
+                                                OctoImage(
+                                                  width: double.infinity,
+                                                  height: double.infinity,
+                                                  placeholderBuilder:
+                                                      OctoBlurHashFix.placeHolder(
+                                                          'LEHV6nWB2yk8pyo0adR*.7kCMdnj'),
+                                                  errorBuilder: OctoError.icon(
+                                                      color: Colors.red),
+                                                  image: CachedNetworkImageProvider(
+                                                      locationController
                                                           .retrievedLocationSearches[
                                                               index]
-                                                          .locationName,
-                                                      fontSize: 25.sp,
-                                                      color: Colors.white),
-                                                  8.ph,
-                                                  // paragraph(
-                                                  //     text:
-                                                  //         '${locationController.retrievedLocationSearches[index].categoryCount} places',
-                                                  //     fontSize: 18.sp,
-                                                  //     color: Colors.white)
-                                                ],
-                                              ),
+                                                          .locationPicture[0]
+                                                          .locationPictureUrl),
+                                                  fit: BoxFit.cover,
+                                                ),
+                                                Container(
+                                                  height: double.infinity,
+                                                  width: double.infinity,
+                                                  decoration: const BoxDecoration(
+                                                      gradient: LinearGradient(
+                                                          begin: Alignment
+                                                              .topCenter,
+                                                          end: Alignment
+                                                              .bottomCenter,
+                                                          colors: [
+                                                        Colors.transparent,
+                                                        Colors.black45
+                                                      ])),
+                                                ),
+                                                Positioned(
+                                                  bottom: 15.h,
+                                                  left: 15.w,
+                                                  right: 15.w,
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      heading(
+                                                          text: locationController
+                                                              .retrievedLocationSearches[
+                                                                  index]
+                                                              .locationName,
+                                                          fontSize: 25.sp,
+                                                          color: Colors.white),
+                                                      8.ph,
+                                                      // paragraph(
+                                                      //     text:
+                                                      //         '${locationController.retrievedLocationSearches[index].categoryCount} places',
+                                                      //     fontSize: 18.sp,
+                                                      //     color: Colors.white)
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
                                             ),
-                                          ],
+                                          ),
                                         ),
-                                      ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                              );
-                            })
-                        .animate(onPlay: (controller) => controller.repeat())
-                        .shimmer(
-                            delay: 700.ms,
-                            duration: 2000.ms,
-                            color: Theme.of(context).scaffoldBackgroundColor)),
+                                  );
+                                })
+                            .animate(
+                                onPlay: (controller) => controller.repeat())
+                            .shimmer(
+                                delay: 700.ms,
+                                duration: 2000.ms,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor)),
             widget.hideBackButton!
                 ? const SizedBox.shrink()
                 : Positioned(
-                    top: 60.h,
+                    top: 40.h,
                     left: 20.w,
                     right: 20.w,
                     child: Row(
@@ -188,10 +190,11 @@ class _SearchResultPageState extends State<SearchResultPage> {
                                 Icon(CupertinoIcons.arrow_left,
                                     color: brandPrimaryColor, size: 30.w),
                                 20.pw,
-                                heading(
+                                title(
                                     text: 'Search Results',
-                                    fontSize: 30.sp,
-                                    color: brandPrimaryColor),
+                                    fontSize: 46.sp,
+                                    color: brandPrimaryColor,
+                                    textAlign: TextAlign.center),
                               ],
                             ),
                           ),
